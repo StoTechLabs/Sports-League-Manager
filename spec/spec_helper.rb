9579fb1f3e4401d5002f884b88594a7c4e83337f
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-27 12:36:45 -0700

    Set up spec_helper folder and create spec helper files.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'league'
+require 'division'
+require 'team'
+require 'game'
+require 'player'
+
+database_configurations = YAML::load(File.open('./db/config.yml'))
+development_configuration = database_configurations['test']
+ActiveRecord::Base.establish_connection(development_configuration)
+
+RSpec.configure do |config|
+  config.after(:each) do
+    League.all.each {|league| league.destroy}
+    Division.all.each {|divsion| divsion.destroy}
+    Team.all.each {|team| team.destroy}
+    Game.all.each {|game| game.destroy}
+    Player.all.each {|player| player.destroy}
+  end
+end
