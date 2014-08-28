2fe62d19036114555e0f3600cdafb7efaba9d8d0
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-28 12:27:42 -0700

    Create spec to test relationship between the Team, Player and Game classes. Spec passes.

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
+require 'pry'
 
 describe Team do
 	it "should show players and games" do
 		test_team = Team.create({:name => 'New Jack City'})		
 		test_player = Player.create({:name => "Nino Brown"})
-		test_game = Game.create({:date => '2014-07-29'})
+		game_date = DateTime.parse('2014-07-29 ')
+		test_game = Game.create({:date => game_date})
 		test_team.players << test_player
-		test_player.games << test_game
-		expect(test_team.players).to eq test_player
-		expect(test_team.game).to eq test_game
+		test_game.teams << test_team
+		expect(test_team.players).to eq [test_player]
+		expect(test_game.teams).to eq [test_team]
 	end
 end
