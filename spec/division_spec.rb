b0e081736e2aba52b4c17cb63f13d13b992034bc
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-28 10:21:36 -0700

    Create spec to test realationship between Division and Team. Spec Passes.

--- spec/division_spec.rb
+++ spec/division_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 
+describe Division do
+	it "should show its teams" do
+		test_division = Division.create({:name => "Division A"})
+		test_team = Team.create({:name => 'New Jack City'})
+		test_division.teams << test_team
+		expect(test_division.teams).to eq [test_team]
+	end
+end
+
+
+
+
