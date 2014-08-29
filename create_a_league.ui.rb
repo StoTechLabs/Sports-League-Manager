b2aff63e64b0da88af1cc123b90cc56e5776cf3f
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-29 12:04:55 -0700

    Create 'League Menu' in user interface.

--- create_a_league.ui.rb
+++ create_a_league.ui.rb
@@ -21,13 +21,13 @@ def main_menu
   puts "Enter the number of a menu choice to continue"
   user_input = gets.chomp
   if user_input == '1'
-    create_league
+    league_menu
   elsif user_input == '2'
-    create_team
+    team_menu
   elsif user_input == '3'
-    create_division
+    division_menu
   elsif user_input == '4'
-  	create_game
+  	game_menu
   elsif user_input == 'x'
     puts "Thanks for using SLM. Have a great day!"
     exit
@@ -35,6 +35,42 @@ def main_menu
     puts "Invalid entry"
     main_menu
   end
+end
+
+def league_menu
+  puts "\n*** LEAGUE MENU ***\n\n"
+  choice = nil
+  until choice == 'e'
+    sleep(1.0)
+    puts "Press 'a' to add a LEAGUE, 'l' to list your leagues, 'r' return to Main menu"
+    puts "Press 'e' to exit"
+    choice = gets.chomp
+    case choice
+    when 'a'
+      puts "Enter the full name of your new league"
+      league_name = gets.chomp
+      puts "Enter the name of the sport that will played in this league."
+      sport_name = gets.chomp
+      new_league = League.create({:name => league_name, :sport => sport_name})
+      sleep(1.0)
+      puts " New league has been added"
+    when 'l'
+      League.all.each { |league| puts league.name }
+      puts "\n\n"
+    when 'r'
+      main_menu
+    when 'e'
+      puts "Bye"
+      exit
+    else
+      puts "That was not a valid entry"
+    end
+  end
+end
+
+def division_menu
+
+
 end
 
 main_menu
